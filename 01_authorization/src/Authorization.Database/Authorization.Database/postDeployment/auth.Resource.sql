4804b2d687891f9186df41e75163a6e9532b5afd
Arab Ait Ouarabi <aaitouarabi@kpmg.fr>
2024-04-19 07:54:46 +0000

    Merged PR 30744: fix(sql): Mise en place de correctifs sur le model
    Related work items: #303984

--- 01_authorization/src/Authorization.Database/Authorization.Database/postDeployment/auth.Resource.sql
+++ 01_authorization/src/Authorization.Database/Authorization.Database/postDeployment/auth.Resource.sql
@@ -1,21 +1,31 @@
 ﻿IF (SELECT count(*) FROM [auth].[Resource]) = 0
 BEGIN
-SET IDENTITY_INSERT [auth].[Resource] OFF; 
-
-	INSERT INTO [auth].[Resource](ResourceId,ParentId, Name, Label, Category, Code, OrderShow) VALUES
-	(6, NULL,'mandat','Mandat', 'globale', 'MND000', 1),
-	(7, NULL,'mod','Demandes MOD', 'globale', 'MOD000', 2),
-	(8, NULL,'collaborators','Collaborateurs', 'globale', 'ACL000', 3),
-	(9, NULL,'history','Historique', 'globale', 'HIS000', 4),
-	(1, NULL,'entity','Dossier', 'navigation', 'ACC000', 1),
-		-- Insert  entity childs
-		(2, 1,'users','Utilisateurs', 'navigation', 'USR000', 2),
-		(3, 1,'collaborators','Collaborateurs', 'navigation', 'CLB000', 3),
-		(4, 1,'offers','Offres', 'navigation', 'OFF000', 4),
-		(5, 1,'informations','Informations', 'navigation', 'INF000', 5),
-	(10, NULL,'documents','Documents', 'navigation', 'GED000', 6),
-	(11, NULL,'bank','Banque', 'navigation', 'BNK001', 7),
-	(12, NULL,'indicators','Indicateurs', 'navigation', 'KPI000', 8)
-
 SET IDENTITY_INSERT [auth].[Resource] ON; 
+
+	INSERT INTO [auth].[Resource](ResourceId,ParentId, Name, Label, Category, Type, Code, OrderShow) VALUES
+		-- Vue Collaborateurs
+	(1, NULL,'mandat','Mandat', 'globale', 'Collaborator', 'MCOL0001', 1),
+	(2, NULL,'mod','Demandes MOD', 'globale','Collaborator', 'MCOL0002', 2),
+	(3, NULL,'collaborators','Collaborateurs', 'globale', 'Collaborator', 'MCOL0003', 3),
+	(4, NULL,'history','Historique', 'globale', 'Collaborator', 'MCOL0004', 4),
+	(5, NULL,'entity','Dossier', 'unitaire', 'Collaborator', 'MCOL0005', 1),
+		-- Insert  entity childs
+		(6, 5,'users','Utilisateurs', 'unitaire','Collaborator', 'MCOL5006', 2),
+		(7, 5,'collaborators','Collaborateurs', 'unitaire','Collaborator', 'MCOL5007', 3),
+		(8, 5,'offers','Offres', 'unitaire', 'Collaborator', 'MCOL5008', 4),
+		(9, 5,'informations','Informations', 'unitaire', 'Collaborator', 'MCOL5009', 5),
+	(10, NULL,'documents','Documents', 'unitaire', 'Collaborator', 'MCOL0019', 6),
+	(11, NULL,'bank','Banque', 'unitaire', 'Collaborator', 'MCOL0011', 7),
+	(12, NULL,'indicators','Indicateurs', 'unitaire', 'Collaborator', 'MCOL0012', 8),
+	-- Vue Client
+	(13, NULL,'entities','Mes entités', 'globale', 'Customer', 'MCUS0001', 1),
+	(14, NULL,'Users','Tous les utilisateurs', 'globale','Customer', 'MCUS0002', 2),
+	(15, NULL,'entity','Dossier', 'unitaire', 'Customer', 'MCUS0003', 1),
+		-- Insert  entity childs
+		(16, 15,'users','Utilisateurs', 'unitaire','Customer', 'MCUS3004', 2),
+		(17, 15,'collaborators','Collaborateurs KPMG', 'unitaire','Customer', 'MCUS3005', 3),
+		(18, 15,'informations','Informations', 'unitaire', 'Customer', 'MCUS3006', 5),
+	(19, NULL,'billing','Factures KPMG', 'unitaire', 'Customer', 'MCUS0007', 6)
+
+SET IDENTITY_INSERT [auth].[Resource] OFF; 
 END
