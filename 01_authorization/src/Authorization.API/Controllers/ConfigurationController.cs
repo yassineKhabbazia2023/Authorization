9659dd4fb572ed16e1b0dc1b72f5d1e1c14b51d3
FR\jnantenaina <jnantenaina@kpmg.fr>
2024-04-29 10:42:07 +0200

    feat: update comments

--- 01_authorization/src/Authorization.API/Controllers/ConfigurationController.cs
+++ 01_authorization/src/Authorization.API/Controllers/ConfigurationController.cs
@@ -37,7 +37,6 @@ public class ConfigurationController : ControllerBase
     /// <summary>
     /// Récupère la configuration des authorizations d'un account.
     /// </summary>
-    /// <param name="contactId">Identifiant du contat.</param>
     /// <param name="accountId">Identifiant de l'entité morale.</param>
     /// <returns>La liste des configurations.</returns>
     [HttpGet]
