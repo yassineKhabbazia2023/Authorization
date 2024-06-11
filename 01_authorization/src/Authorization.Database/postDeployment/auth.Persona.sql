ebe5bca79dbfd73a90910a405244534435ea7d1e
FR\jnantenaina <jnantenaina@kpmg.fr>
2024-06-11 15:29:59 +0200

    feat: add signataire

--- 01_authorization/src/Authorization.Database/postDeployment/auth.Persona.sql
+++ 01_authorization/src/Authorization.Database/postDeployment/auth.Persona.sql
@@ -22,5 +22,6 @@ BEGIN
 		(19, N'MOD Forms', N'', 1),
 		(20, N'Collaborateur BI', N'', 1),
 		(21, N'Collaborateur', N'', 1),
-		(22, N'Client', N'', 2)
+		(22, N'Client', N'', 2),
+		(23, N'Signataire', N'', 2)
 END
