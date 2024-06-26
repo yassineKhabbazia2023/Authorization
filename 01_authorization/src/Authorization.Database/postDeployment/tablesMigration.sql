49d8d92ac8d91f672f3d01fb78f4b67f4d13fb10
Jerry Nantenaina <jnantenaina@kpmg.fr>
2024-06-26 13:36:46 +0000

    Merged PR 32625: feat(auth): add transco
    feat(auth): add transco
    
    Related work items: #324485
    
    (cherry picked from commit c9bf179d0fc4f01d9e4822c36aac062e5f479122)

--- 01_authorization/src/Authorization.Database/postDeployment/tablesMigration.sql
+++ 01_authorization/src/Authorization.Database/postDeployment/tablesMigration.sql
@@ -40,4 +40,12 @@ INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('RIGH32' , 'CLEVP
 INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('RIGH32' , 'CLEMB001' )
 INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('DS0006' , 'CLGED0001' )
 INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('DS0005' , 'CLGED0002' )
+INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('DS0006', 'COGED0001')
+INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('DS0005', 'COGED0002')
+INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('RIGH14', 'COLANC001')
+INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('RIGH32', 'COLANC001')
+INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('RIGH11', 'COLANC001')
+INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('RIGH33', 'COLANC001')
+INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('RIGH31', 'COKPI0001')
+INSERT INTO Transco (AccessRightCode, AuthorizationId) VALUES ('RIGH07', 'COBANK001')
 END
