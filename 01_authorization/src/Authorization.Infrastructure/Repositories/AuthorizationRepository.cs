4981b67f3b3f3040c62a5491aa7a58bc571acdd8
NGUYEN Bao Trung <baotrungnguyen@rydge.fr>
2025-06-30 13:58:09 +0000

    Merged PR 5158: fix: retourne que les contacts avec role et permission particulier dans un ac...
    fix: retourne que les contacts avec role et permission particulier dans un account
    
    Related work items: #49350

--- 01_authorization/src/Authorization.Infrastructure/Repositories/AuthorizationRepository.cs
+++ 01_authorization/src/Authorization.Infrastructure/Repositories/AuthorizationRepository.cs
@@ -423,7 +423,7 @@ public class AuthorizationRepository : IAuthorizationRepository
 
         var query = _authorizationContext.ContactAuthorizationEntity
                         .AsNoTracking()
-                        .Where(ca => authorizationIdRequired.Contains(ca.AuthorizationId))
+                        .Where(ca => authorizationIdRequired.Contains(ca.AuthorizationId) && ca.AccountId == accountId)
                         .GroupBy(ca => ca.ContactId)
                         .Where(g => g.Select(ca => ca.AuthorizationId).Distinct().Count() == codes.Count)
                         .Select(g => g.Key);
