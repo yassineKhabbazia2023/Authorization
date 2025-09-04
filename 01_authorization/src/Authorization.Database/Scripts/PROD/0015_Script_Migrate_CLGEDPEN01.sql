e1b488957643925f3f4a356d40c93aa149acde00
BEN AMOR Oussema <obenamor@rydge.fr>
2025-09-04 13:10:09 +0000

    Merged PR 6241: 🔨 Migrate right CLGEDPEN01
    🔨 Migrate right CLGEDPEN01
    
    Give the access right CLGEDPEN01  to the list of users that have the access right CLPEN001
    
    Refs: #52043
    
    Tests 🧪
    Initial state of the user without the access right **CLGEDPEN01**
    ![image.png](https://dev.azure.com/FR-TPEME/fb2de3b3-d9ff-42db-abfe-2a7530c44fad/_apis/git/repositories/b5673af6-3967-4d8e-860f-bd4f8cf60317/pullRequests/6241/attachments/image.png)
    
    Script execution log
    ![image (2).png](https://dev.azure.com/FR-TPEME/fb2de3b3-d9ff-42db-abfe-2a7530c44fad/_apis/git/repositories/b5673af6-3967-4d8e-860f-bd4f8cf60317/pullRequests/6241/attachments/image%20%282%29.png)
    
    > _**was updated to display account id too**_
    > ![image (3).png](https://dev.azure.com/FR-TPEME/fb2de3b3-d9ff-42db-abfe-2a7530c44fad/_apis/git/repositories/b5673af6-3967-4d8e-860f-bd4f8cf60317/pullRequests/6241/attachments/image%20%283%29.png)
    
    Results:
    ![image (4).png](https://dev.azure.com/FR-TPEME/fb2de3b3-d9ff-42db-abfe-2a7530c44fad/_apis/git/repositories/b5673af6-3967-4d8e-860f-bd4f8cf60317/pullRequests/6241/attachments/image%20%284%29.png)
    ![image (5).png](https://dev.azure.com/FR-TPEME/fb2de3b3-d9ff-42db-abfe-2a7530c44fad/_apis/git/repositories/b5673af6-3967-4d8e-860f-bd4f8cf60317/pullRequests/6241/attachments/image%20%285%29.png)
    
    Related work items: #52043

---
+++ 01_authorization/src/Authorization.Database/Scripts/PROD/0015_Script_Migrate_CLGEDPEN01.sql
@@ -0,0 +1,57 @@
+﻿BEGIN TRY
+    BEGIN TRAN;
+
+    -- Parameters
+    DECLARE @ssoPennylaneCode  varchar(50) = 'CLPEN001';
+    DECLARE @CLGEDPEN01        varchar(50) = 'CLGEDPEN01';
+
+    -- Resolve AuthorizationIds once
+    DECLARE @ssoId int, @penId int;
+
+    SELECT @ssoId = AuthorizationId
+    FROM [auth].[Authorization]
+    WHERE Code = @ssoPennylaneCode;
+
+    SELECT @penId = AuthorizationId
+    FROM [auth].[Authorization]
+    WHERE Code = @CLGEDPEN01;
+
+    -- Capture inserted rows
+    DECLARE @Inserted TABLE (
+        ContactId      int,
+        AccountId      int,
+        AuthorizationId int,
+        CreationDate   datetime
+    );
+
+    -- Insert the missing CLGEDPEN01 authorization and log inserted rows
+    INSERT INTO auth.ContactAuthorization (ContactId, AccountId, AuthorizationId, CreationDate)
+    OUTPUT inserted.ContactId, inserted.AccountId, inserted.AuthorizationId, inserted.CreationDate
+    INTO @Inserted (ContactId, AccountId, AuthorizationId, CreationDate)
+    SELECT DISTINCT ca.ContactId,
+                    ca.AccountId,
+                    @penId,
+                    GETDATE()
+    FROM auth.ContactAuthorization AS ca
+    WHERE ca.AuthorizationId = @ssoId
+      AND NOT EXISTS (
+            SELECT 1
+            FROM auth.ContactAuthorization AS x
+            WHERE x.ContactId = ca.ContactId
+              AND x.AccountId = ca.AccountId
+              AND x.AuthorizationId = @penId
+      );
+
+    COMMIT TRAN;
+
+    SELECT DISTINCT ContactId, AccountId
+    FROM @Inserted
+    ORDER BY ContactId;
+
+END TRY
+BEGIN CATCH
+    IF XACT_STATE() <> 0
+        ROLLBACK TRAN;
+
+    THROW;
+END CATCH;
