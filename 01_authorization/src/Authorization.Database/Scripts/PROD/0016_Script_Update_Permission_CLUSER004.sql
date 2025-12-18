d15b7503c59608c5178367674a74760436c55461
NAITSI Ayoub <anaitsi@rydge.fr>
2025-12-18 08:59:08 +0000

    Merged PR 7319: Script modification CLUSER004
    Script modification CLUSER004
    
    Related work items: #56351

---
+++ 01_authorization/src/Authorization.Database/Scripts/PROD/0016_Script_Update_Permission_CLUSER004.sql
@@ -0,0 +1,13 @@
+﻿UPDATE [auth].[Authorization]
+SET [View] = 'Partial'
+WHERE [View] = 'Both'
+  AND [Code] = 'CLUSER004';
+GO
+DELETE FROM auth.AccountAuthorization
+WHERE AccountId = -1
+  AND AuthorizationId = (
+        SELECT AuthorizationId
+        FROM [auth].[Authorization]
+        WHERE Code = 'CLUSER004'
+  );
+GO
